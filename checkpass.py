2fb2080f616c1e776e6e0390de447b53600101e8
firebaker <eccolosky@gmail.com>
2015-07-25 22:20:47 -0500

    add checkpass.py, in isotherms.py add algorithms to fit isotherms to experimental data

---
+++ checkpass.py
@@ -0,0 +1,12 @@
+import math
+import numpy as np
+
+
+def _check_nan(item):
+    for val in item:
+        if math.isnan(val):
+            raise Exception
+
+
+def _verify_numeric(item):
+    for 
--- isotherms.py
+++ isotherms.py
@@ -1,3 +1,10 @@
+from scipy.optimize import curve_fit
+from .checkpass import _check_nan
+
+
+""" bare isotherm equations """
+
+
 # Linear isotherm equation
 def linearIsotherm(x, Kd):
     return Kd * x
@@ -11,3 +18,36 @@ def freundlichIsotherm(x, Kf, n):
 # Langmuir isotherm equation and related functions
 def langmuirIsotherm(x, Smax, Kl):
     return Smax * Kl * x / (1 + Kl * x)
+
+
+"""" algorithms to fit isotherms to experimental results"""
+
+
+# fit linear isotherm
+def fitLinear(x, y, Kd):
+    try:
+        popt, pcov = curve_fit(linearIsotherm, x, y, p0=(Kd))
+        _check_nan(popt)
+        return popt, pcov
+    except:
+        return None, 'warning: unable to fit linear isotherm'
+
+
+# fit freundlich isotherm
+def fitFreundlich(x, y, Kf, n):
+    try:
+        popt, pcov = curve_fit(freundlichIsotherm, x, y, p0=(Kf, n))
+        _check_nan(popt)
+        return popt, pcov
+    except:
+        return None, 'warning: unable to fit linear isotherm'
+
+
+# fit langmuir isotherm
+def fitLangmuir(x, y, Smax, Kl):
+    try:
+        popt, pcov = curve_fit(langmuirIsotherm, x, y, p0=(Smax, Kl))
+        _check_nan(popt)
+        return popt, pcov
+    except:
+        return None, 'warning: unable to fit linear isotherm'
