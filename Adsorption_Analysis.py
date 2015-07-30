31f1d53260a5f269e96030dbb55b60401d1027a5
firebaker <eccolosky@gmail.com>
2015-07-30 15:51:59 -0500

    refactor fitIsotherm() functions
    - modify Adsorption_Analysis.py -> create fitIsotherm(), reduce
      individual fitIsotherm functions
    - modify stats.py -> modify to work with fitIsotherm()

--- Adsorption_Analysis.py
+++ Adsorption_Analysis.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+# note: curve_fit returns popt and pcov as arrays not list
 from scipy.optimize import curve_fit
 from dataStructure import _chk_key_dict
 from checkPass import _check_nan
@@ -11,48 +12,50 @@ import background_Adsorption_Analysis as bAA
 
 """"
 algorithms to fit isotherms to experimental results using
-scipy.optimize curve_fit; return popt and pcov variables
-for each isotherm in an output dictionary
+scipy.optimize curve_fit; return popt (list) and pcov (2d list)
+variables for each isotherm in an output dictionary
 """
+
+
+# generic fit isotherm function (optionally user defined);
+# return popt, pcov, SSR
+def fitIsotherm(
+        isotherm, x, y, p0, checkInput=True, isoName='user-defined'):
+    if checkInput:
+        bAA.checkAdsorptionInput(x, y)
+        x = np.float64(x)
+        y = np.float64(y)
+    try:
+        popt, pcov = curve_fit(isotherm, x, y, p0)
+        _check_nan(popt)
+        SSR = _regSSR(isotherm, x, y, popt)
+        # convert popt and pcov from np.array to list
+        popt_return = np.array(popt).tolist()
+        pcov_return = np.array(pcov).tolist()
+        return {'popt': popt_return, 'pcov': pcov_return, 'SSR': SSR}
+    except:
+        return {'warning': 'unable to fit %s isotherm' % isoName}
 
 
 # fit linear isotherm; return popt, pcov, SSR
 def fitLinear(x, y, Kd=1, checkInput=True):
-    if checkInput:
-        bAA.checkAdsorptionInput(x, y)
-    try:
-        popt, pcov = curve_fit(bAA.linearIsotherm, x, y, p0=(Kd))
-        _check_nan(popt)
-        SSR = _regSSR(bAA.linearIsotherm, x, y, popt)
-        return {'popt': popt, 'pcov': pcov, 'SSR': SSR}
-    except:
-        return {'warning': 'unable to fit linear isotherm'}
+    p0 = (Kd)
+    return fitIsotherm(
+        bAA.linearIsotherm, x, y, p0, checkInput, 'linear')
 
 
 # fit freundlich isotherm, return popt, pcov, SSR
 def fitFreundlich(x, y, Kf=1, n=1, checkInput=True):
-    if checkInput:
-        bAA.checkAdsorptionInput(x, y)
-    try:
-        popt, pcov = curve_fit(bAA.freundlichIsotherm, x, y, p0=(Kf, n))
-        _check_nan(popt)
-        SSR = _regSSR(bAA.freundlichIsotherm, x, y, popt)
-        return {'popt': popt, 'pcov': pcov, 'SSR': SSR}
-    except:
-        return {'warning': 'unable to fit freundlich isotherm'}
+    p0 = (Kf, n)
+    return fitIsotherm(
+        bAA.freundlichIsotherm, x, y, p0, checkInput, 'freundlich')
 
 
 # fit langmuir isotherm, return popt, pcov, SSE
 def fitLangmuir(x, y, Qmax=1, Kl=1, checkInput=True):
-    if checkInput:
-        bAA.checkAdsorptionInput(x, y)
-    try:
-        popt, pcov = curve_fit(bAA.langmuirIsotherm, x, y, p0=(Qmax, Kl))
-        _check_nan(popt)
-        SSR = _regSSR(bAA.langmuirIsotherm, x, y, popt)
-        return {'popt': popt, 'pcov': pcov, 'SSR': SSR}
-    except:
-        return {'warning': 'unable to fit langmuir isotherm'}
+    p0 = (Qmax, Kl)
+    return fitIsotherm(
+        bAA.langmuirIsotherm, x, y, p0, checkInput, 'langmuir')
 
 
 def adsorptionAnalysis(
@@ -85,7 +88,9 @@ def adsorptionAnalysis(
 
 # guess Qmax, optional for fitLangmuir(); average y value of x-axis
 # endpoints
-def guessQmax(x, y, xndpts=3):
+def guessQmax(x, y, xndpts=3, checkInput=True):
+    if checkInput:
+        bAA.checkAdsorptionInput(x, y)
     xlist = sorted(range(len(x)), key=lambda i: x[i])[-xndpts:]
     ylist = []
     for index in xlist:
--- stats.py
+++ stats.py
@@ -4,6 +4,6 @@
 # calculate linear/non-linear regression SSR (sum of squared residuals)
 # requires 'popt' variable from from curve_fit()
 def _regSSR(func, x, y, popt):
-    residuals = y - func(x, popt)
+    residuals = y - func(x, *popt)
     SSR = sum(residuals**2)
     return SSR
